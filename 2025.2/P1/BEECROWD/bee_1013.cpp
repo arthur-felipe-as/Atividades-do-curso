acdafe19b583df72ddef26cc9b0c353faf9c3623
arthur-felipe-as <arthur.f.andrade.s@gmail.com>
2025-08-19 08:40:47 -0300

    atividades do beecrowd

---
+++ 2025.2/P1/BEECROWD/bee_1013.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int maior_ab(int a, int b){
+    return (a+b+abs(a-b))/2;
+}
+
+int main(){
+    int a{};
+    int b{};
+    int c{};
+    std::cin >> a >> b >> c;
+    std::cout << maior_ab(maior_ab(a,b),c) << " eh o maior\n";
+    return 0;
+}
---
+++ 2025.2/P1/BEECROWD/bee_1015.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+struct Ponto{
+    float x,y;
+};
+
+float distancia(Ponto p1, Ponto p2){
+    return sqrt(pow(2,(p2.x - p1.x)) + pow(2, (p2.y - p1.y)));
+}
+
+
+int main(){
+    
+    Ponto p1;
+    Ponto p2;
+    std::cin >> p1.x >> p1.y >> p2.x >> p2.y;
+    std::cout << std::fixed << std::setprecision(4) << distancia(p1,p2) << '\n';
+
+
+    return 0;    
+}
