82333920e8db0cd56eb5e70b10ac271be0ac18e9
arthur-felipe-as <arthur.f.andrade.s@gmail.com>
2025-10-09 12:21:46 -0300

    shell sort correto, colocar no principal depoisgit add .!

--- 2025.2/EDB1/sorting/shell.cpp
+++ 2025.2/EDB1/sorting/shell.cpp
@@ -25,32 +25,36 @@ void print(const std::string_view& msg,
    }
  }
 
-void insert_s(value_type* first, value_type* last, value_type* insert, value_type h,  Compare cmp = std::less<>()){
+void insert_s(value_type* first, value_type* last,value_type* insert,int h,  Compare cmp = std::less<>()){
     auto key{*insert};
-    auto it{insert};
-    while (it > first && cmp(key, *(it - 1))) {
-        *it = *(it - 1);
-        it = it-h-1;
+    auto pos{insert};
+    while (pos > first && cmp(key, *(pos - h))) {
+        *pos = *(pos - h);
+        pos -= h;
     }
-    *it = key;
+    *pos = key;
 }
 
-void insertsort_s(value_type* first, value_type* last, value_type h, Compare cmp = std::less<>()){
+void insertsort_s(value_type* first, value_type* last,int h, Compare cmp = std::less<>()){
     if(std::distance(first,last) <= 1){
         return;
     }
-    insertsort_s(first, std::prev(last),h, cmp);
-    insert_s(first, std::prev(std::prev(last)), std::prev(last),h,cmp);
+    insertsort_s(first, last-h,h, cmp);
+    insert_s(first, last-h-h, last -h,h,cmp);
     return;
 }
 
 
 void shellsort(value_type* first,value_type* last, Compare cmp = std::less<>()){
-  auto fast{first};
     for(int h = std::distance(first,last)/2; h > 0; h /= 2){
+      std::cout << "gap tamanho " << h << std::endl;
+      auto fast{first};
+      int count{0};
         while(fast+h != last){
-          insertsort_s(first,last,h,cmp);
+          std::cout << "array numero " << count << std::endl;
+          insertsort_s(fast,last,h,cmp);
           fast++;
+          count++;
         }
     }
     
